fb26ced39b13d833c048b0d33f158c6c185c46cb
ABC <abc@ABCs-MacBook-Pro.local>
2021-11-17 16:15:21 +0530

    netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import  adapter  from '@sveltejs/adapter-netlify';
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  adapter:adapter()
 })
